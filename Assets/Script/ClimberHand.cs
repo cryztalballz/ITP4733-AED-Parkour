2c6d5bc1059867d838408159bc0c02717da45a8c
Recomt <80800723+Recomt@users.noreply.github.com>
2021-11-26 13:03:10 +0800

    update

---
+++ Assets/Script/ClimberHand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Valve.VR;
+public class ClimberHand : MonoBehaviour
+{
+    public SteamVR_Input_Sources Hand;
+    public int TouchedCount;
+    public bool grabbing;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ClimbingAdapter"))
+        {
+            TouchedCount++;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("ClimbingAdapter"))
+        {
+            TouchedCount--;
+        }
+    }
+}
--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -6,20 +6,12 @@ using Valve.VR;
 public class Movement : MonoBehaviour
 {
     public GameObject player;
-    public GameObject hand;
     public SteamVR_Action_Boolean frontType;
     public SteamVR_Action_Boolean leftType;
     public SteamVR_Action_Boolean rightType;
     public SteamVR_Action_Boolean backType;
-    public SteamVR_Action_Boolean grab;
     public SteamVR_Input_Sources type;
     [SerializeField]int speed;
-    Vector3 handPos, distance;
-    bool grabed, recorded;
-    private void Start()
-    {
-        grabed = recorded = false;
-    }
     void Update()
     {
         if (frontType.GetState(type))
@@ -46,35 +38,5 @@ public class Movement : MonoBehaviour
             verticalZero.y = 0;
             player.transform.position = verticalZero;
         }
-        if (grab.GetLastStateDown(type))
-        {
-            grabed = true;
-        }
-        if (grab.GetLastStateUp(type))
-        {
-            grabed = false;
-        }
-    }
-    private void OnTriggerStay(Collider obj)
-    {
-        if (obj.gameObject.CompareTag("ClimbingAdapter"))
-        {
-            if (grab.GetLastStateDown(type))
-            {
-                Debug.Log("grabed");
-                if (!recorded)
-                {
-                    handPos = hand.transform.position;
-                    recorded = true;
-                }
-                distance = hand.transform.position - handPos;
-                player.transform.position += distance;
-            }
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        recorded = false;
-        handPos = distance = Vector3.zero;
     }
 }
