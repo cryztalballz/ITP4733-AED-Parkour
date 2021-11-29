c2939f474250d0a220eb62cfe66c40f8c36414e3
cryztalballz <80933302+cryztalballz@users.noreply.github.com>
2021-11-29 16:35:14 +0800

    update

---
+++ Assets/Script/AedPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AedPosition : MonoBehaviour
+{
+    public Transform correctPosition;   //
+    //public static int PADNumber = 0;
+    public string objName;
+
+    private void Start()
+    {
+        correctPosition = GetComponent<Transform>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == objName)
+        {
+            Debug.Log("I found AED");
+            other.transform.position = new Vector3(correctPosition.position.x, correctPosition.position.y, correctPosition.position.z);
+            other.transform.rotation = new Quaternion(correctPosition.rotation.x, correctPosition.rotation.y, correctPosition.rotation.z, correctPosition.rotation.w);
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            Debug.Log("AED is ok");
+            //PADNumber++;
+            //Debug.Log("PADNumber = " + PADNumber);
+        }
+
+
+    }
+}
