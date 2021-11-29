d994a6ff45830699f13585fb1d69aafe33a4da28
Recomt <80800723+Recomt@users.noreply.github.com>
2021-11-29 17:16:18 +0800

    Update (TankDestroy)

---
+++ Assets/Script/TankBehaviour.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class TankBehaviour : MonoBehaviour {
+
+	public float life=1;
+	public AudioClip hitSound;
+	public AudioClip collectSound;
+	public AudioClip dropSound;
+
+	AudioSource AS;
+
+	void Start () {
+		AS = GetComponent<AudioSource>();
+	}
+
+	void Hit (float damage) {
+		life -= damage;
+		ScoreCount.UpdateScore(1);
+		if (life > 0)
+			AS.PlayOneShot(hitSound);
+		else
+			Kill ();
+	}
+
+	void Kill () {
+		AudioSource.PlayClipAtPoint(collectSound, transform.position);
+		ScoreCount.UpdateScore(5);
+		Destroy(gameObject);
+	}
+}
