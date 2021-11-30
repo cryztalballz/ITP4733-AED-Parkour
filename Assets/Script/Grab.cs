650d41455f389ad467aba4621a62c8785fa4f910
cryztalballz <mices123@yahoo.com.hk>
2021-11-30 09:00:48 +0800

    update

---
+++ Assets/Script/Grab.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+
+public class Grab : MonoBehaviour
+{
+    public SteamVR_Input_Sources Type;
+    public SteamVR_Action_Boolean GrabGrip;
+    public SteamVR_Behaviour_Pose ControllerPose;
+    public GameObject CameraRig;
+    [SerializeField]
+    public Rigidbody CameraRigConstraints;
+    private bool IsGrabed = false;
+    private static int GrabCount = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (GrabGrip.GetLastStateUp(Type))
+        {
+            IsGrabed = false;
+            CameraRig.GetComponent<Rigidbody>().constraints = CameraRigConstraints.constraints;
+            Release();
+        }
+
+        if (IsGrabed)
+        {
+            CameraRig.GetComponent<Rigidbody>().velocity = -ControllerPose.GetVelocity();
+            CameraRig.GetComponent<Rigidbody>().angularVelocity = new Vector3(200 * ControllerPose.GetAngularVelocity().x,
+                200 * ControllerPose.GetAngularVelocity().y);
+        }
+
+        if (GrabCount <= 0)
+        {
+            CameraRig.GetComponent<Rigidbody>().useGravity = true;
+        }
+        else
+        {
+            CameraRig.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    public bool GetGrab()
+    {
+        return GrabGrip.GetState(Type);
+    }
+    public void Release()
+    {
+        if (GrabCount > 0)
+            GrabCount--;
+    }
+
+    public void OnTriggerStay(Collider col)
+    {
+        if (IsGrabed && col.tag == "GrabUpPoint")
+        {
+            CameraRig.transform.position = col.transform.position;
+        }
+        if (!IsGrabed)
+        {
+            if (col.tag == "wall")
+            {
+                // first grab
+                if (GetGrab())
+                {
+                    GrabCount++;
+                    CameraRig.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+                    IsGrabed = true;
+                }
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "wall")
+        {
+            IsGrabed = false;
+            CameraRig.GetComponent<Rigidbody>().constraints = CameraRigConstraints.constraints;
+            Release();
+        }
+    }
+}
---
+++ Assets/Script/Run.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Run : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public SteamVR_Input_Sources Type;
+    public SteamVR_Action_Boolean Trigger;
+    public SteamVR_Behaviour_Pose RightControllerPose;
+    public SteamVR_Behaviour_Pose LeftControllerPose;
+    public Transform Camera;
+    public Transform Direction;
+    private RigidbodyConstraints constraints;
+    private Rigidbody rigidbody;
+    private bool isChecked;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Trigger.GetLastStateDown(Type))
+        {
+            Debug.Log("set checked true");
+            isChecked = true;
+            constraints = rigidbody.constraints;
+        }
+
+        if (Trigger.GetLastStateUp(Type))
+        {
+            Debug.Log("set checked false");
+            isChecked = false;
+            rigidbody.constraints = constraints;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isChecked)
+        {
+            Debug.Log("checked");
+            Vector3 direction = Direction.transform.forward;
+            Vector3 worldDirection = Camera.transform.TransformDirection(direction);
+            if (RightControllerPose.GetVelocity().y > 0.1f || RightControllerPose.GetVelocity().y <= 0.1f)
+            {
+                Vector3 velocity = RightControllerPose.GetVelocity();
+                if (velocity.y < 0)
+                    velocity.y *= -1;
+                worldDirection *= velocity.y * speed;
+                rigidbody.constraints = RigidbodyConstraints.FreezePositionY | constraints;
+            }
+            if (LeftControllerPose.GetVelocity().y > 0.1f || LeftControllerPose.GetVelocity().y <= 0.1f)
+            {
+                Vector3 velocity = LeftControllerPose.GetVelocity();
+                if (velocity.y < 0)
+                    velocity.y *= -1;
+                worldDirection *= velocity.y * speed;
+                rigidbody.constraints = RigidbodyConstraints.FreezePositionY | constraints;
+            }
+            GetComponent<Rigidbody>().velocity = worldDirection;
+        }
+    }
+}
